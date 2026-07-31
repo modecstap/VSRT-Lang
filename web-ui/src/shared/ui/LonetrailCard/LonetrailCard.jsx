91752ea2933fc36453ce46341775e115dba98e20
Modecstap <toni.shebanin@mail.ru>
2026-08-01 02:52:07 +0300

    refactor(ui): extract universal style theme

--- web-ui/src/shared/ui/LonetrailCard/LonetrailCard.jsx
+++ web-ui/src/shared/ui/LonetrailCard/LonetrailCard.jsx
@@ -2,16 +2,16 @@ import styles from "./LonetrailCard.module.css";
 
 export default function LonetrailCard({ children }) {
   return (
-    <div className={styles.lonetrailWrapper}>
-      <div className={styles.lonetrailTopOberation} />
+    <div className={styles.wrapper}>
+      <div className={styles.topDecoration} />
 
-      <div className={styles.lonetrailCard}>
-        <div className={styles.lonetrailTricolor} />
+      <div className={styles.card}>
+        <div className={styles.accentBar} />
 
         {children}
       </div>
 
-      <div className={styles.lonetrailBottomOberation} />
+      <div className={styles.bottomDecoration} />
     </div>
   );
 }
