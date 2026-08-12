7964451617fb09ccc520a0122ac87f9889f5696b
Modecstap <toni.shebanin@mail.ru>
2026-08-12 12:33:23 +0300

    refactor(main): streamline database setup and handler initialization

--- GO-Manager/cmd/auth/main.go
+++ GO-Manager/cmd/auth/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"VSRT-Lang/internal"
 	"VSRT-Lang/internal/auth"
+	"VSRT-Lang/internal/database/postgres"
 	"VSRT-Lang/internal/database/postgres/migrations"
 	"VSRT-Lang/internal/database/postgres/refresh_token_repository"
 	"VSRT-Lang/internal/database/postgres/session_repository"
@@ -21,34 +22,53 @@ import (
 func main() {
 	host := "localhost:8080"
 
-	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=vsrt_lang sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+	db := setupDb()
+
+	cors, mux := setupServer(db)
+
+	http.ListenAndServe(host, cors(mux))
+}
+
+func setupDb() *sql.DB {
+	dbConfig, err := postgres.LoadDBConfig()
 	if err != nil {
 		panic(err)
 	}
-	if err := migrations.Run(db); err != nil {
+
+	db, err := sql.Open("postgres", dbConfig.ConnString())
+	if err != nil {
 		panic(err)
 	}
 
+	err = migrations.Run(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func setupServer(db *sql.DB) (func(http.Handler) http.Handler, *http.ServeMux) {
+	userRepo := user_repository.New(db)
+	tokenRepo := refresh_token_repository.New(db)
+	jwtService := auth.NewJWTService("StrongSecretString")
 	service := auth.NewService(
-		user_repository.New(db),
-		refresh_token_repository.New(db),
-		auth.NewJWTService("StrongSecretString"),
+		userRepo,
+		tokenRepo,
+		jwtService,
 	)
 	authHandler := auth_handler.NewAuth(service)
 
 	sessionRepo := session_repository.New(db)
-	sessionHandler := session_handler.NewHandler(sessionRepo, internal.MockTranslator{})
-
-	userRepo := user_repository.New(db)
+	sessionTranslator := internal.MockTranslator{}
+	sessionHandler := session_handler.NewHandler(sessionRepo, sessionTranslator)
 
 	userHandler := user_handler.NewHandler(userRepo, sessionRepo)
 
 	handlers := myHttp.Handlers{
-		Auth:                 authHandler,
-		Session:            sessionHandler,
-		User:                  userHandler,
-		AuthMiddleware: middleware.Auth(auth.NewJWTService("StrongSecretString")),
+		Auth:           authHandler,
+		Session:        sessionHandler,
+		User:           userHandler,
+		AuthMiddleware: middleware.Auth(jwtService),
 	}
 
 	cors := middleware.CORS(middleware.CORSConfig{
@@ -70,6 +90,5 @@ func main() {
 	})
 
 	mux := myHttp.NewServeMux(handlers)
-
-	http.ListenAndServe(host, cors(mux))
+	return cors, mux
 }
