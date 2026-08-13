6040011847ec740106323803a73e4298aad7fbad
Shebanin <toni.shebanin@mail.ru>
2026-08-13 17:29:47 +0300

    refactor(main): delete old main command

--- GO-Manager/cmd/main.go
+++ GO-Manager/cmd/main.go
@@ -1,78 +1,83 @@
 package main
 
 import (
-	"VSRT-Lang/internal"
+	"VSRT-Lang/internal/database/postgres"
 	"VSRT-Lang/internal/database/postgres/migrations"
-	"VSRT-Lang/internal/database/postgres/session_repository"
-	"VSRT-Lang/internal/session"
+	myHttp "VSRT-Lang/internal/http"
+	router "VSRT-Lang/internal/http"
+	auth_handler "VSRT-Lang/internal/http/handlers/auth"
+	session_handler "VSRT-Lang/internal/http/handlers/session"
+	user_handler "VSRT-Lang/internal/http/handlers/user"
+	"VSRT-Lang/internal/http/middleware"
 	"database/sql"
-	"fmt"
+	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
-// TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//translator := net_translator.Translator{
-	//	Client: http.Client{
-	//		Transport:     nil,
-	//		CheckRedirect: nil,
-	//		Jar:           nil,
-	//		Timeout:       0,
-	//	},
-	//	Backend: "http://localhost:8080/api/translator",
-	//}
+	host := "localhost:8080"
 
-	connStr := "host=localhost port=5432 user=postgres password=postgres dbname=vsrt_lang sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-	if err := migrations.Run(db); err != nil {
-		panic(err)
-	}
-	repository := session_repository.New(db)
+	db := setupDb()
 
-	testSession := session.Session{
-		User:       "",
-		Name:       "test",
-		Records:    nil,
+	secret, ok := os.LookupEnv("SECRET_KEY")
+	if !ok {
+		panic("environment variable SECRET_KEY not found")
 	}
 
-	translator := internal.MockTranslator{}
+	deps := router.New(db, secret)
 
-	err = repository.Save(&testSession)
-	if err != nil {
-		panic(err)
-	}
+	cors, mux := setupServer(deps)
 
-	record := testSession.SaveRecord(
-		"simple sentence to show work of app",
-		"simple sentence",
-		translator,
-	)
-
-	printRecord(record)
-
-	err = repository.Save(&testSession)
-
-	taken, err := repository.Take(0)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Print("\n ========== FROM DB ========== \n\n")
-
-	for _, takeRecord := range taken.Records {
-		printRecord(takeRecord)
-	}
-
+	http.ListenAndServe(host, cors(mux))
 }
 
-func printRecord(takeRecord session.Record) {
-	fmt.Printf("Phrase:\t\t%s\n", takeRecord.Phrase)
-	fmt.Printf("Translations:\t%s\n", takeRecord.Translations)
-	fmt.Printf("BaseForm:\t%s\n", takeRecord.BaseForm)
-	fmt.Printf("Synonyms:\t%s\n", takeRecord.Synonyms)
-	fmt.Printf("Antonyms:\t%s\n", takeRecord.Antonyms)
-	fmt.Printf("Contexts:\t%s\n", takeRecord.Contexts)
+func setupDb() *sql.DB {
+	dbConfig, err := postgres.LoadDBConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open("postgres", dbConfig.ConnString())
+	if err != nil {
+		panic(err)
+	}
+
+	err = migrations.Run(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func setupServer(deps *router.ServerDependens) (func(http.Handler) http.Handler, *http.ServeMux) {
+	handlers := myHttp.Handlers{
+		Auth:           auth_handler.NewAuth(deps.AuthService),
+		Session:        session_handler.NewHandler(deps.SessionRepo, deps.Translator),
+		User:           user_handler.NewHandler(deps.UserRepo, deps.SessionRepo),
+		AuthMiddleware: middleware.Auth(deps.JwtService),
+	}
+
+	cors := middleware.CORS(middleware.CORSConfig{
+		AllowedOrigins: []string{
+			"*",
+		},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodDelete,
+			http.MethodOptions,
+		},
+		AllowedHeaders: []string{
+			"Content-Type",
+			"Authorization",
+		},
+		AllowCredentials: true,
+	})
+
+	mux := myHttp.NewServeMux(handlers)
+	
+	return cors, mux
 }
