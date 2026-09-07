b39b36a9b4763396233bde4f9ada1dfd1aca18c0
modecstap <toni.shebanin@mail.ru>
2026-09-07 14:16:15 +0300

    fix(translator): incorrect import NTLK.wordnet

--- Translator/tests/test_translator.py
+++ Translator/tests/test_translator.py
@@ -1,11 +1,15 @@
 import pytest
 
+from translator.models.word import Word
 from translator.translator import Translator
 
 
 @pytest.fixture(scope="session")
 def translator():
-    return Translator()
+    return Translator(
+        url="http://localhost:5000",
+        key=""
+    )
 
 
 @pytest.mark.parametrize(
@@ -34,3 +38,16 @@ def test_translation_bulk(translator: Translator, originals: list[str]):
         assert translation.original == originals[i]
         assert isinstance(translation.translations, list)
         assert len(translation.translations) > 0
+
+
+@pytest.mark.parametrize(
+    "target", [
+        Word(content="test")
+    ]
+)
+def test_synonyms(translator: Translator, target: Word):
+    synonyms = translator.take_synonyms(target)
+
+    assert isinstance(synonyms, list)
+    for synonym in synonyms:
+        assert isinstance(synonym, Word)
