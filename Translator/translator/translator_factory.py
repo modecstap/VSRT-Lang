d55ca3089da0a30b92d268ce4026ce4271ec18e6
modecstap <toni.shebanin@mail.ru>
2026-09-07 15:37:44 +0300

    feat(translator): factory try to reconnect when url is unavailable

--- Translator/translator/translator_factory.py
+++ Translator/translator/translator_factory.py
@@ -1,7 +1,12 @@
 import os
+from time import sleep
 
+from translator import translator
 from translator.i_translator import ITranslator
 from translator.translator import Translator
+
+
+TIME_BEFORE_RECONNECT = 50 # in seconds
 
 
 class TranslatorFactory:
@@ -14,4 +19,14 @@ class TranslatorFactory:
         url = "http://"+url+"/"
         print(url)
         key = os.getenv("LIBRETRANSLATE_API_KEY", "")
-        return Translator(url, key)
+
+        translator = None
+
+        while not translator:
+            try:
+                translator = Translator(url, key)
+            except Exception as e:
+                print(e)
+                sleep(TIME_BEFORE_RECONNECT)
+
+        return translator
